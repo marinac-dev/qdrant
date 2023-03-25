7b049a6519011fdb90ca252f04e78669c3c72844
marinac-dev <github@regex-sh.com>
2023-03-25 14:02:15 +0100

    chore: Add tesla as HTTP client
    Signed-off-by: marinac-dev <github@regex-sh.com>

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Qdrant.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.29.3"}
+      {:ex_doc, "~> 0.29.3"},
+      {:tesla, "~> 1.5"}
     ]
   end
 
