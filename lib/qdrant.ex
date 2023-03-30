b9aad5e1049a9947903aecdd485bcd3b5920fd57
marinac-dev <github@regex-sh.com>
2023-03-30 20:43:21 +0200

    feat(qdrant): Add qdrant applicaiton
    Signed-off-by: marinac-dev <github@regex-sh.com>

--- lib/qdrant.ex
+++ lib/qdrant.ex
@@ -1,5 +1,18 @@
 defmodule Qdrant do
   @moduledoc """
-  Documentation for `Qdrant`.
+  Documentation for Qdrant.
   """
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      {Qdrant.Tokenizer, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: Qdrant.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
