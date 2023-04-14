07e6e98f782c8ca54f53bc08811ed5031f2f9263
marinac-dev <github@regex-sh.com>
2023-04-14 02:05:26 +0200

    feat(qdrant): Add Api Wrapper module
    Signed-off-by: marinac-dev <github@regex-sh.com>

---
+++ lib/qdrant/api/wrapper.ex
@@ -0,0 +1,22 @@
+defmodule Qdrant.Api.Wrapper do
+  defmacro __using__(_opts) do
+    quote do
+      defp module_name(module) do
+        case Application.get_env(:qdrant, :interface) do
+          "rest" -> Module.concat(["Qdrant", "Api", "Http", module])
+          "grpc" -> Module.concat(["Qdrant", "Api", "Grpc", module])
+          _ -> raise "Invalid interface configuration for Qdrant"
+        end
+      end
+
+      import(Qdrant.Api.Wrapper, only: [api_call: 3])
+    end
+  end
+
+  @doc false
+  defmacro api_call(module, fn_name, params) do
+    quote do
+      module_name(unquote(module)) |> apply(unquote(fn_name), unquote(params))
+    end
+  end
+end
